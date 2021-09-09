135224a7ad903766802a69ccd1fee91af112b384
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-09-09 18:12:22 -0300

    C2 - Adicionado requisito 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar'
 import MovieList from './components/MovieList';
 import movies from './data';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
       <MovieList movies={ movies } />
     </div>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,22 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
+
+    return (
+      <h2>oiiiiiiiiiiiiiiiiiii</h2>
+    );
+  }
+}
+
+// SearchBar.propTypes = {
+
+// };
+export default SearchBar;
